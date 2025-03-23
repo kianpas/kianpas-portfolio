bc81035fa65d51113a29faab85f63a468fbc866d
kianpas <kianpas@gmail.com>
2025-03-23 22:50:03 +0900

    about 헤더 색 수정, 로그인 링크 추가가

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,7 +4,6 @@ import { useState } from "react";
 import Link from "next/link";
 
 const Navbar = () => {
-
   // 모바일시 햄버거 메뉴 클릭 여부
   const [isOpen, setIsOpen] = useState(false);
   return (
@@ -43,36 +42,36 @@ const Navbar = () => {
             </Link> */}
         </div>
 
-          <div className="md:hidden">
-            <button
-              onClick={() => setIsOpen(!isOpen)}
-              className="text-gray-600 focus:outline-none"
+        <div className="md:hidden">
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            className="text-gray-600 focus:outline-none"
+          >
+            <svg
+              className="h-6 w-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
-              <svg
-                className="h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                {isOpen ? (
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                ) : (
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                )}
-              </svg>
-            </button>
-          </div>
+              {isOpen ? (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              ) : (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              )}
+            </svg>
+          </button>
+        </div>
       </div>
 
       {/* Mobile Links */}
@@ -93,6 +92,12 @@ const Navbar = () => {
               className="block text-gray-800 hover:text-blue-500"
             >
               Blog
+            </Link>
+            <Link
+              href="/login"
+              className="block text-gray-800 hover:text-blue-500"
+            >
+              Login
             </Link>
           </div>
         </div>
