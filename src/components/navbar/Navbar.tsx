5cde4850345ecc396da08f0bcadbb432baaef9aa
kianpas <kianpas@gmail.com>
2025-04-01 22:16:56 +0900

    네비바 모바일 대응 수정정

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -7,10 +7,10 @@ const Navbar = () => {
   // 모바일시 햄버거 메뉴 클릭 여부
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <nav className="shadow-sm fixed top-0 w-full z-10 h-16">
+    <nav className="bg-white dark:bg-gray-900 shadow-sm fixed top-0 w-full z-50 h-16">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center h-16">
         <div className="flex items-center">
-          <Link href="/" className="text-xl font-semibold text-gray-800">
+          <Link href="/" className="text-xl font-semibold">
             LOGO
           </Link>
         </div>
@@ -18,25 +18,25 @@ const Navbar = () => {
         <div className="hidden md:flex space-x-6">
           <Link
             href="/"
-            className="text-xl hover:text-gray-200 transition-colors duration-200"
+            className="text-xl hover:text-gray-700 dark:hover:text-gray-300 transition-colors duration-200"
           >
             Home
           </Link>
           <Link
             href="/blog"
-            className="text-xl hover:text-gray-200 transition-colors duration-200"
+            className="text-xl hover:text-gray-700 dark:hover:text-gray-300 transition-colors duration-200"
           >
             Blog
           </Link>
           <Link
               href="/project"
-              className="text-xl hover:text-gray-200 transition-colors duration-200"
+              className="text-xl hover:text-gray-700 dark:hover:text-gray-300 transition-colors duration-200"
             >
               Project
             </Link>
           <Link
             href="/about"
-            className="text-xl hover:text-gray-200 transition-colors duration-200"
+            className="text-xl hover:text-gray-700 dark:hover:text-gray-300 transition-colors duration-200"
           >
             About
           </Link>
@@ -45,7 +45,7 @@ const Navbar = () => {
         <div className="md:hidden">
           <button
             onClick={() => setIsOpen(!isOpen)}
-            className="text-gray-600 focus:outline-none"
+            className="text-gray-600 dark:text-gray-300 focus:outline-none"
           >
             <svg
               className="h-6 w-6"
@@ -76,32 +76,32 @@ const Navbar = () => {
 
       {/* Mobile Links */}
       {isOpen && (
-        <div className="md:hidden bg-white shadow-md">
+        <div className="md:hidden bg-white dark:bg-gray-900 shadow-md">
           <div className="px-4 pt-2 pb-4 space-y-2">
-            <Link href="/" className="block text-gray-800 hover:text-blue-500">
+            <Link href="/" className="block text-gray-800 dark:text-white hover:text-blue-500">
               Home
             </Link>
             <Link
               href="/blog"
-              className="block text-gray-800 hover:text-blue-500"
+              className="block text-gray-800 dark:text-white hover:text-blue-500"
             >
               Blog
             </Link>
             <Link
               href="/project"
-              className="block text-gray-800 hover:text-blue-500"
+              className="block text-gray-800 dark:text-white hover:text-blue-500"
             >
               Project
             </Link>
             <Link
               href="/about"
-              className="block text-gray-800 hover:text-blue-500"
+              className="block text-gray-800 dark:text-white hover:text-blue-500"
             >
               About
             </Link>
             <Link
               href="/login"
-              className="block text-gray-800 hover:text-blue-500"
+              className="block text-gray-800 dark:text-white hover:text-blue-500"
             >
               Login
             </Link>
